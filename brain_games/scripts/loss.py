a908956d45acd2aff98b3309f4fcbcbe57338f1b
Chudilo4 <chipalinchik@gmail.com>
2022-07-02 10:17:17 +0500

    поправил модуль loss

--- brain_games/scripts/loss.py
+++ brain_games/scripts/loss.py
@@ -2,4 +2,4 @@
 
 def loss(an_tr, an_us, name):
     print(f'\'{an_us}\' is wrong answer ;(. Correct answer was\'{an_tr}\'.')
-    print(f'Let\'s try again, {name}')
+    print(f'Let\'s try again, {name}!')
