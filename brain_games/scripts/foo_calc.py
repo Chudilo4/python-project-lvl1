d62a848bdd8d119e5c0bd04b5ad08e1615330384
Chudilo4 <chipalinchik@gmail.com>
2022-07-02 17:11:39 +0300

    добавли пробелы между числами в вопросе

--- brain_games/scripts/foo_calc.py
+++ brain_games/scripts/foo_calc.py
@@ -8,11 +8,11 @@ def foo_calc(rn, rn2):
     sym = ('+', '-', '*')
     rn3 = choice(sym)
     if rn3 == '+':
-        print(f'Question: {rn}{rn3}{rn2}')
+        print(f'Question: {rn} {rn3} {rn2}')
         return rn + rn2
     elif rn3 == '-':
-        print(f'Question: {rn}{rn3}{rn2}')
+        print(f'Question: {rn} {rn3} {rn2}')
         return rn - rn2
     else:
-        print(f'Question: {rn}{rn3}{rn2}')
+        print(f'Question: {rn} {rn3} {rn2}')
         return rn * rn2
