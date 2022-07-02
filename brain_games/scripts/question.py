75c13dda3913a2db5dab8be49691d790c7a8253f
Chudilo4 <chipalinchik@gmail.com>
2022-07-02 17:13:25 +0300

    убрал условие при котором функция реализовывается если в ней прописано название игры

--- brain_games/scripts/question.py
+++ brain_games/scripts/question.py
@@ -7,20 +7,19 @@ from brain_games.scripts.loss import loss
 from brain_games.scripts.foo_calc import foo_calc
 
 
-def question(txt, name):
-    if txt == 'brain_calc':
-        count = 0
-        answr_true = 0
-        answr_user = 0
-        while count <= 2:
-            r_n = randint(0, 100)
-            r_n2 = randint(0, 100)
-            answr_true = foo_calc(r_n, r_n2)
-            answr_user = prompt.string('Your answer: ')
-            if int(answr_user) == answr_true:
-                print('Correct')
-                count += 1
-            else:
-                loss(answr_true, answr_user, name)
-                break
-            print(f'Congratulations, {name}!')
+def question(name):
+    count = 0
+    answr_true = 0
+    answr_user = 0
+    while count <= 2:
+        r_n = randint(0, 100)
+        r_n2 = randint(0, 100)
+        answr_true = foo_calc(r_n, r_n2)
+        answr_user = prompt.string('Your answer: ')
+        if int(answr_user) == answr_true:
+            print('Correct')
+            count += 1
+        else:
+            loss(answr_true, answr_user, name)
+            break
+        print(f'Congratulations, {name}!')
